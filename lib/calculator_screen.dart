e13054814b933f43e7e822a088e79b024879e5a8
WojciechBabik <faccomim@gmail.com>
2025-03-05 10:26:53 +0100

    Template

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bibik_kalkulator/button_values.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorScreen extends StatefulWidget {
@@ -10,20 +11,56 @@ class CalculatorScreen extends StatefulWidget {
 class CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         bottom: false,
         child: Column(
           children: [
             //output
-            Text(
-              "0",
-              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            Expanded(
+              child: SingleChildScrollView(
+                reverse: true,
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  padding: const EdgeInsets.all(15),
+                  child: Text(
+                    "0",
+                    style: const TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+              ),
             ),
-            //przyciski
+            //input
+            Wrap(
+              children:
+                  Btn.buttonValues
+                      .map(
+                        (value) => SizedBox(
+                          width: screenSize.width / 4,
+                          height: screenSize.width / 5,
+                          child: buildButton(value),
+                        ),
+                      )
+                      .toList(),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildButton(value) {
+    return Material(
+      shape: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(120),
+        borderSide: BorderSide(color: Colors.white30),
+      ),
+      child: InkWell(onTap: () {}, child: Center(child: Text(value))),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'calculator_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
